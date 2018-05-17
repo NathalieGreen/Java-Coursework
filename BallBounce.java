3682e607ad7b26b7b5b53cb2769335bf043a80df
NathalieGreen <n.green@lancaster.ac.uk>
2018-05-17 12:30:52 +0100

    Ball Bounce 2nd round
    When the balls reach the bottom of the screen, it resets the location of the balls back to the centre launching point.

--- BallBounce.java
+++ BallBounce.java
@@ -1,33 +1,38 @@
 public class BallBounce
 {
    Ball b;
-   double xSpeed = 5;
-   double ySpeed = -6;
-   static int id =0;
+   private double xSpeed = 5;
+   private double ySpeed = -6;
+   private boolean isBallLost = false;
+  
    public BallBounce(GameArena arena){
       b= new Ball(385,560, 10, "FireBrick");      
-      id++;
       arena.addBall(b);
+      //System.out.println(id);
    }
+  
+   private Text ballcount = new Text("X 15", 875, 553, 15, "black");
+ 
+   
    public void ballmove(Rectangle[] bricks, GameArena arena, Text[] numbers, Rectangle R, Line l, Ball bc){
       double BallX = b.getXPosition();      //ballX=X coordinate of the ball
       double BallY = b.getYPosition();      //bally=y coordinate of the ball
-    
+      arena.addText(ballcount);
       
       if (BallX> 755){
          xSpeed = -xSpeed;
          b.setXPosition(755 + xSpeed);
-         //System.out.println(xSpeed + "  "+BallX);
       }
       else if( BallX<10){
 		  xSpeed = -xSpeed;
-		   b.setXPosition(10 + xSpeed);
+		   b.setXPosition(10 + xSpeed);		   
 	  }
 		  
 		  
       if (BallY>= 585){
-         ySpeed = -ySpeed;
-          b.setYPosition(585 - ySpeed);
+         //ySpeed = -ySpeed;
+          //b.setYPosition(585 - ySpeed);
+          isBallLost=true;                    
       }
       else if( BallY<10){
 		  ySpeed = -ySpeed;
@@ -45,19 +50,20 @@ public class BallBounce
 		   (BallX> (bricks[i].getXPosition()-25) && 
 		   (BallX< (bricks[i].getXPosition()+25)))){
 			hit = true;
+	
 			
-			arena.removeText (numbers[i]);
+			//arena.removeText (numbers[i]);			//deletes the current number on the brick
 			double numPosX = numbers[i].getXPosition();
 			double numPosY = numbers[i].getYPosition();
 			int temp = Integer.parseInt(numbers[i].getText());
-			if (temp > 0){
-				temp = temp-1;
-				numbers[i] = new Text ("" + Integer.toString(temp), numPosX,numPosY, 15, "White");
-				
-				arena.addText (numbers[i]);
+			if (temp > 0){							//needs this if statement to ensure that the numbers don't go in to minus numbers
+				temp = temp-1;						//minuses 1 to the current number on the brick
+				//numbers[i] = new Text ("" + Integer.toString(temp), numPosX,numPosY, 15, "White");		//creates a new number which is -1 to what it was before
+				numbers[i].setText("" + Integer.toString(temp));
+				//arena.addText (numbers[i]);			//adds the new number onto the bricks
 				arena.update();
 		
-				//if it is between the Y borders of the brick, then it has hit one of the X borders 		
+					//bounces the ball off of the side of the brick
 				if( Math.abs(BallX-bricks[i].getXPosition()) > Math.abs(BallY-bricks[i].getYPosition()) ) {
 					//BallX = BallX-xSpeed;
 					xSpeed=-xSpeed;
@@ -68,6 +74,8 @@ public class BallBounce
 				}
 				 
 				 
+				 
+				 //sets the colour of the brick dependent on the new number on it
 				if ((temp>=10) && (temp<=15)){
 				   bricks[i].setColour("Indigo");
 				}
@@ -88,54 +96,55 @@ public class BallBounce
 		}
 		i++;
 		
+		
+		//if(BallY==600){
+			//System.out.println("You have hit the bottom of the screen");
+		//}
+		
+		
+		
 		//if the gold squares are hit, all of the surrounding squares dissapear too.
-			if ((BallY> (bricks[30].getYPosition()-25) && 
+		if ((BallY> (bricks[30].getYPosition()-25) && 
 				(BallY< (bricks[30].getYPosition()+25))) && 
 				(BallX> (bricks[30].getXPosition()-25) && 
 				(BallX< (bricks[30].getXPosition()+25)))){
-					arena.removeRectangle(bricks[30]);
-					arena.removeText(numbers[30]);
-					arena.removeRectangle(bricks[11]);
-					arena.removeText(numbers[11]);
-					arena.removeRectangle(bricks[12]);
-					arena.removeText(numbers[12]);
-					arena.removeRectangle(bricks[13]);
-					arena.removeText(numbers[13]);
-					arena.removeRectangle(bricks[29]);
-					arena.removeText(numbers[29]);
-					arena.removeRectangle(bricks[31]);
-					arena.removeText(numbers[31]);
-					arena.removeRectangle(bricks[47]);
-					arena.removeText(numbers[47]);
-					arena.removeRectangle(bricks[48]);
-					arena.removeText(numbers[48]);
-					arena.removeRectangle(bricks[49]);
-					arena.removeText(numbers[49]);
+					blowup(bricks, numbers, 30, arena);
+					blowup(bricks, numbers, 11, arena);
+					blowup(bricks, numbers, 12, arena);
+					blowup(bricks, numbers, 13, arena);
+					blowup(bricks, numbers, 29, arena);
+					blowup(bricks, numbers, 31, arena);
+					blowup(bricks, numbers, 47, arena);
+					blowup(bricks, numbers, 48, arena);
+					blowup(bricks, numbers, 49, arena);
 			}  
 			
-			if ((BallY> (bricks[59].getYPosition()-25) && 
+		if ((BallY> (bricks[59].getYPosition()-25) && 
 				(BallY< (bricks[59].getYPosition()+25))) && 
 				(BallX> (bricks[59].getXPosition()-25) && 
 				(BallX< (bricks[59].getXPosition()+25)))){
-					arena.removeRectangle(bricks[59]);
-					arena.removeText(numbers[59]);
-					arena.removeRectangle(bricks[40]);
-					arena.removeText(numbers[40]);
-					arena.removeRectangle(bricks[41]);
-					arena.removeText(numbers[41]);
-					arena.removeRectangle(bricks[42]);
-					arena.removeText(numbers[42]);
-					arena.removeRectangle(bricks[58]);
-					arena.removeText(numbers[58]);
-					arena.removeRectangle(bricks[60]);
-					arena.removeText(numbers[60]);
-					arena.removeRectangle(bricks[76]);
-					arena.removeText(numbers[76]);
-					arena.removeRectangle(bricks[77]);
-					arena.removeText(numbers[77]);
-					arena.removeRectangle(bricks[78]);
-					arena.removeText(numbers[78]);
-			}   
+					blowup(bricks, numbers, 40, arena);
+					blowup(bricks, numbers, 41, arena);
+					blowup(bricks, numbers, 42, arena);
+					blowup(bricks, numbers, 58, arena);
+					blowup(bricks, numbers, 59, arena);
+					blowup(bricks, numbers, 60, arena);
+					blowup(bricks, numbers, 76, arena);
+					blowup(bricks, numbers, 77, arena);
+					blowup(bricks, numbers, 78, arena);
+		}   
+		
+		if ((BallY> (bricks[88].getYPosition()-25) && 
+				(BallY< (bricks[88].getYPosition()+25))) && 
+				(BallX> (bricks[88].getXPosition()-25) && 
+				(BallX< (bricks[88].getXPosition()+25)))){
+					blowup(bricks, numbers, 88, arena);
+					blowup(bricks, numbers, 89, arena);
+					blowup(bricks, numbers, 87, arena);
+					blowup(bricks, numbers, 70, arena);
+					blowup(bricks, numbers, 69, arena);
+					blowup(bricks, numbers, 71, arena);
+		}   
 			 
 		
 	  }
@@ -150,4 +159,32 @@ public class BallBounce
 	   ySpeed = yPosition;
 	   
 	}
+	
+	//a method which blows a square up
+	public void blowup(Rectangle[] bricks,Text[] numbers, int index, GameArena arena){
+		arena.removeRectangle(bricks[index]);
+		arena.removeText(numbers[index]);
+		numbers[index].setText("0");
+	}
+	
+	
+	public boolean getIsBallLost(){
+		return isBallLost;
+	}
+	
+	public void resetLocation(){
+		b.setXPosition(385);
+		b.setYPosition(560);
+	}
+
+	public void resetXSpeed(){
+		//b.setXPosition(385);
+		xSpeed = 0;
+	}
+	
+	public void resetYSpeed(){
+		//b.setYPosition(560);
+		ySpeed=0;
+	}
 }
+
--- CreateBricks.java
+++ CreateBricks.java
@@ -1,47 +1,55 @@
 import java.util.Random;
-import java.awt.Font;
 
 public class CreateBricks
 {
-	Random rand = new Random(); 
+   Random rand = new Random();
 
-	public void createBricks(Rectangle[] bricks, GameArena arena, Text[] numbers, Rectangle R, Line l)
-	{
-	    arena.addRectangle (R);
-	    //arena.addBall(b);
-	    arena.addLine(l);
-	    //arena.addBallBounce(bb);
-	    //arena.addArrow(arrow);
-		int index =0;
-		
-		
-		for (int r=0; r<5; r++){
-			for (int c=0; c<17; c++){
-			    int randomNumber = rand.nextInt(15);
-                bricks[index] = new Rectangle(30+45*c,30+45*r, 40, 40, "Blue");
-                numbers[index] = new Text ("" + randomNumber, 30+45*c,30+45*r, 15, "White");
-				arena.addRectangle (bricks[index]);
-				arena.addText (numbers[index]);
+   public void createBricks(Rectangle[] bricks, GameArena arena, Text[] numbers, Rectangle R, Line l, Ball bc)
+   {
+      arena.addRectangle (R);
+      arena.addBall(bc);
+      arena.addLine(l);
 
-				if ((randomNumber>=10) && (randomNumber<=15)){
-					bricks[index].setColour("Indigo");
-				}
-				else if ((randomNumber>=5) && (randomNumber<10)){
-                    bricks[index].setColour("DarkViolet");
-				}
-				else if ((randomNumber>=0)&& (randomNumber<5)){
-                    bricks[index].setColour("Violet");
-                    numbers[index].setColour("Black");
-				}
-
-				if(randomNumber==0){
-				    arena.removeRectangle(bricks[index]);
-                }
-
-                index ++;
-
+      int index =0;
+      //this is a for loop which creates rows and columns to layout the bricks and to set random numbers on each brick
+      for (int r=0; r<5; r++){
+         for (int c=0; c<18; c++){
+            int randomNumber = rand.nextInt(15);
+            bricks[index] = new Rectangle(30+41*c,30+41*r, 40, 40, "Blue");
+            numbers[index] = new Text ("" + randomNumber, 30+41*c,30+41*r, 15, "White");
+            arena.addRectangle (bricks[index]);
+            arena.addText (numbers[index]);
+            
+			//if statements to set the colour of the brick corresponding to which number is on it
+            if ((randomNumber>=10) && (randomNumber<=15)){
+               bricks[index].setColour("Indigo");
             }
-		}
-	}
-	
+            else if ((randomNumber>=5) && (randomNumber<10)){
+               bricks[index].setColour("DarkViolet");
+            }
+            else if ((randomNumber>0)&& (randomNumber<5)){
+               bricks[index].setColour("Violet");
+               numbers[index].setColour("Black");
+            }
+            
+
+            if(randomNumber==0){
+               arena.removeRectangle(bricks[index]);
+               arena.removeText(numbers[index]);
+            }
+            
+           
+
+            index ++;
+
+         }
+      }
+      //sets specific bricks as Gold as they are the magic bricks
+       bricks[30].setColour("Gold");
+       numbers[30].setColour("Purple");
+       bricks[59].setColour("Gold");
+       numbers[59].setColour("Purple");
+       bricks[88].setColour("Gold");
+       numbers[88].setColour("Purple");
+   }
 }

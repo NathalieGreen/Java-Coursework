ee9e15cab64ccdbdf471853e853a94822d730ca9
NathalieGreen <n.green@lancaster.ac.uk>
2018-05-21 21:49:25 +0100

    Game now allows multiple rounds.

--- BallBounce.java
+++ BallBounce.java
@@ -1,189 +1,189 @@
 public class BallBounce
 {
-   Ball b;
-   private double xSpeed = 5;
-   private double ySpeed = -6;
-   private boolean isBallLost = false;
-  
-   public BallBounce(GameArena arena){
-      b= new Ball(385,560, 10, "FireBrick");      
-      arena.addBall(b);
-      //System.out.println(id);
-   }
-  
-   private Text ballcount = new Text("X 15", 875, 553, 15, "black");
- 
-   
-   public void ballmove(Rectangle[] bricks, GameArena arena, Text[] numbers, Rectangle R, Line l, Ball bc){
-      double BallX = b.getXPosition();      //ballX=X coordinate of the ball
-      double BallY = b.getYPosition();      //bally=y coordinate of the ball
-      arena.addText(ballcount);
-      
-      if (BallX> 755){
-         xSpeed = -xSpeed;
-         b.setXPosition(755 + xSpeed);
-      }
-      else if( BallX<10){
-		  xSpeed = -xSpeed;
-		   b.setXPosition(10 + xSpeed);		   
-	  }
-		  
-		  
-      if (BallY>= 585){
-         //ySpeed = -ySpeed;
-          //b.setYPosition(585 - ySpeed);
-          isBallLost=true;                    
-      }
-      else if( BallY<10){
-		  ySpeed = -ySpeed;
-		   b.setYPosition(10 - ySpeed);
-	  }
-      
-      
-      int i=0;
-      boolean hit = false;
-      while(hit==false && i<90){
-		//this section says that if the ball hits the bricks, then the number 
-		//printed on the bricks needs to decrease by 1  
-		if ((BallY> (bricks[i].getYPosition()-25) && 
-		   (BallY< (bricks[i].getYPosition()+25))) && 
-		   (BallX> (bricks[i].getXPosition()-25) && 
-		   (BallX< (bricks[i].getXPosition()+25)))){
-			hit = true;
-	
-			
-			//arena.removeText (numbers[i]);			//deletes the current number on the brick
-			double numPosX = numbers[i].getXPosition();
-			double numPosY = numbers[i].getYPosition();
-			int temp = Integer.parseInt(numbers[i].getText());
-			if (temp > 0){							//needs this if statement to ensure that the numbers don't go in to minus numbers
-				temp = temp-1;						//minuses 1 to the current number on the brick
-				//numbers[i] = new Text ("" + Integer.toString(temp), numPosX,numPosY, 15, "White");		//creates a new number which is -1 to what it was before
-				numbers[i].setText("" + Integer.toString(temp));
-				//arena.addText (numbers[i]);			//adds the new number onto the bricks
-				arena.update();
-		
-					//bounces the ball off of the side of the brick
-				if( Math.abs(BallX-bricks[i].getXPosition()) > Math.abs(BallY-bricks[i].getYPosition()) ) {
-					//BallX = BallX-xSpeed;
-					xSpeed=-xSpeed;
-				}
-				else {
-					//BallY = BallY-ySpeed;
-					ySpeed=-ySpeed;
-				}
-				 
-				 
-				 
-				 //sets the colour of the brick dependent on the new number on it
-				if ((temp>=10) && (temp<=15)){
-				   bricks[i].setColour("Indigo");
-				}
-				else if ((temp>=5) && (temp<10)){
-				   bricks[i].setColour("DarkViolet");
-				}
-				else if ((temp>0)&& (temp<5)){
-				   bricks[i].setColour("Violet");
-				   numbers[i].setColour("Black");
-				}
+	Ball b;
+	private double xSpeed = 5;
+	private double ySpeed = -6;
+	private boolean isBallLost = false;
+	//private int score = 0;
 
-				if(temp==0){
-				   arena.removeRectangle(bricks[i]);
-				   arena.removeText(numbers[i]);
-				    numbers[i].setColour("Black");
+	public BallBounce(GameArena arena){
+		b= new Ball(385,560, 10, "FireBrick");
+		arena.addBall(b);
+		//System.out.println(id);
+	}
+
+	private Text ballcount = new Text("X 15", 875, 553, 15, "black");
+
+
+
+
+	public void ballmove(Rectangle[] bricks, GameArena arena, Text[] numbers, Rectangle R, Line l, Ball bc){
+		double BallX = b.getXPosition();      //X coordinate of the ball
+		double BallY = b.getYPosition();      //y coordinate of the ball
+		arena.addText(ballcount);
+
+
+		//stops the balls from bouncing over the edges
+		if (BallX> 755){
+			xSpeed = -xSpeed;
+			b.setXPosition(755 + xSpeed);
+		}
+		else if( BallX<10){
+			xSpeed = -xSpeed;
+			b.setXPosition(10 + xSpeed);
+		}
+
+		if (BallY>= 585){
+			//ySpeed = -ySpeed;
+			//b.setYPosition(585 - ySpeed);
+			isBallLost=true;
+		}
+		else if( BallY<10){
+			ySpeed = -ySpeed;
+			b.setYPosition(10 - ySpeed);
+		}
+
+
+		int i=0;
+		boolean hit = false;
+		while(hit==false && i<90){
+			//System.out.println(score);
+			//if the ball hits the bricks, then the number
+			//printed on the bricks needs to decrease by 1
+			if ((BallY> (bricks[i].getYPosition()-25) &&
+					(BallY< (bricks[i].getYPosition()+25))) &&
+					(BallX> (bricks[i].getXPosition()-25) &&
+							(BallX< (bricks[i].getXPosition()+25)))){
+				hit = true;
+
+
+				//arena.removeText (numbers[i]);			//deletes the current number on the brick
+				double numPosX = numbers[i].getXPosition();
+				double numPosY = numbers[i].getYPosition();
+				int temp = Integer.parseInt(numbers[i].getText());  	//creates a new temp int which is the previous number
+				if (temp > 0){							//needs this statement to ensure that the numbers don't go in to minus numbers
+					temp = temp-1;						//minuses 1 to the current number on the brick
+					numbers[i].setText("" + Integer.toString(temp));  //
+					arena.update();
+
+					//bounces the ball off of the sides of the brick
+					if( Math.abs(BallX-bricks[i].getXPosition()) > Math.abs(BallY-bricks[i].getYPosition()) ) {
+						xSpeed=-xSpeed;
+					}
+					else {
+						ySpeed=-ySpeed;
+					}
+
+					//sets the colour of the brick dependent on the new number on it
+					if ((temp>=10) && (temp<=15)){
+						bricks[i].setColour("Indigo");
+					}
+					else if ((temp>=5) && (temp<10)){
+						bricks[i].setColour("DarkViolet");
+					}
+					else if ((temp>0)&& (temp<5)){
+						bricks[i].setColour("Violet");
+						numbers[i].setColour("Black");
+					}
+
+					if(temp==0){
+						arena.removeRectangle(bricks[i]);
+						arena.removeText(numbers[i]);
+						numbers[i].setColour("Black");
+					}
 				}
 			}
-		}
-		i++;
-		
-		
-		//if(BallY==600){
+			i++;
+
+
+			//if(BallY==600){
 			//System.out.println("You have hit the bottom of the screen");
-		//}
-		
-		
-		
-		//if the gold squares are hit, all of the surrounding squares dissapear too.
-		if ((BallY> (bricks[30].getYPosition()-25) && 
-				(BallY< (bricks[30].getYPosition()+25))) && 
-				(BallX> (bricks[30].getXPosition()-25) && 
-				(BallX< (bricks[30].getXPosition()+25)))){
-					blowup(bricks, numbers, 30, arena);
-					blowup(bricks, numbers, 11, arena);
-					blowup(bricks, numbers, 12, arena);
-					blowup(bricks, numbers, 13, arena);
-					blowup(bricks, numbers, 29, arena);
-					blowup(bricks, numbers, 31, arena);
-					blowup(bricks, numbers, 47, arena);
-					blowup(bricks, numbers, 48, arena);
-					blowup(bricks, numbers, 49, arena);
-			}  
-			
-		if ((BallY> (bricks[59].getYPosition()-25) && 
-				(BallY< (bricks[59].getYPosition()+25))) && 
-				(BallX> (bricks[59].getXPosition()-25) && 
-				(BallX< (bricks[59].getXPosition()+25)))){
-					blowup(bricks, numbers, 40, arena);
-					blowup(bricks, numbers, 41, arena);
-					blowup(bricks, numbers, 42, arena);
-					blowup(bricks, numbers, 58, arena);
-					blowup(bricks, numbers, 59, arena);
-					blowup(bricks, numbers, 60, arena);
-					blowup(bricks, numbers, 76, arena);
-					blowup(bricks, numbers, 77, arena);
-					blowup(bricks, numbers, 78, arena);
-		}   
-		
-		if ((BallY> (bricks[88].getYPosition()-25) && 
-				(BallY< (bricks[88].getYPosition()+25))) && 
-				(BallX> (bricks[88].getXPosition()-25) && 
-				(BallX< (bricks[88].getXPosition()+25)))){
-					blowup(bricks, numbers, 88, arena);
-					blowup(bricks, numbers, 89, arena);
-					blowup(bricks, numbers, 87, arena);
-					blowup(bricks, numbers, 70, arena);
-					blowup(bricks, numbers, 69, arena);
-					blowup(bricks, numbers, 71, arena);
-		}   
-			 
-		
-	  }
-      
-      b.setXPosition (BallX+xSpeed);
-      b.setYPosition (BallY+ySpeed);
-  }
-   
-   public void setXSpeed(double xPosition, double yPosition){
-	   //xSpeed= -ySpeed*(xPosition-385)/yPosition;
-	   xSpeed = xPosition;
-	   ySpeed = yPosition;
-	   
+			//}
+
+
+
+			//if the gold squares are hit, all of the surrounding squares dissapear too.
+			if ((BallY> (bricks[30].getYPosition()-25) &&
+					(BallY< (bricks[30].getYPosition()+25))) &&
+					(BallX> (bricks[30].getXPosition()-25) &&
+							(BallX< (bricks[30].getXPosition()+25)))){
+				blowup(bricks, numbers, 30, arena);
+				blowup(bricks, numbers, 11, arena);
+				blowup(bricks, numbers, 12, arena);
+				blowup(bricks, numbers, 13, arena);
+				blowup(bricks, numbers, 29, arena);
+				blowup(bricks, numbers, 31, arena);
+				blowup(bricks, numbers, 47, arena);
+				blowup(bricks, numbers, 48, arena);
+				blowup(bricks, numbers, 49, arena);
+			}
+
+			if ((BallY> (bricks[59].getYPosition()-25) &&
+					(BallY< (bricks[59].getYPosition()+25))) &&
+					(BallX> (bricks[59].getXPosition()-25) &&
+							(BallX< (bricks[59].getXPosition()+25)))){
+				blowup(bricks, numbers, 40, arena);
+				blowup(bricks, numbers, 41, arena);
+				blowup(bricks, numbers, 42, arena);
+				blowup(bricks, numbers, 58, arena);
+				blowup(bricks, numbers, 59, arena);
+				blowup(bricks, numbers, 60, arena);
+				blowup(bricks, numbers, 76, arena);
+				blowup(bricks, numbers, 77, arena);
+				blowup(bricks, numbers, 78, arena);
+			}
+
+			if ((BallY> (bricks[88].getYPosition()-25) &&
+					(BallY< (bricks[88].getYPosition()+25))) &&
+					(BallX> (bricks[88].getXPosition()-25) &&
+							(BallX< (bricks[88].getXPosition()+25)))){
+				blowup(bricks, numbers, 88, arena);
+				blowup(bricks, numbers, 89, arena);
+				blowup(bricks, numbers, 87, arena);
+				blowup(bricks, numbers, 70, arena);
+				blowup(bricks, numbers, 69, arena);
+				blowup(bricks, numbers, 71, arena);
+			}
+
+
+		}
+
+		b.setXPosition (BallX+xSpeed);
+		b.setYPosition (BallY+ySpeed);
 	}
-	
+
+
+	public void setXSpeed(double xPosition, double yPosition){
+		//xSpeed= -ySpeed*(xPosition-385)/yPosition;
+		xSpeed = xPosition;
+		ySpeed = yPosition;
+
+	}
+
 	//a method which blows a square up
 	public void blowup(Rectangle[] bricks,Text[] numbers, int index, GameArena arena){
 		arena.removeRectangle(bricks[index]);
 		arena.removeText(numbers[index]);
 		numbers[index].setText("0");
 	}
-	
-	
+
+
 	public boolean getIsBallLost(){
 		return isBallLost;
 	}
-	
+
 	public void resetLocation(){
 		b.setXPosition(385);
 		b.setYPosition(560);
+
+		isBallLost = false;
 	}
 
 	public void resetXSpeed(){
-		//b.setXPosition(385);
 		xSpeed = 0;
 	}
-	
+
 	public void resetYSpeed(){
-		//b.setYPosition(560);
 		ySpeed=0;
 	}
 }
--- NewGame.java
+++ NewGame.java
@@ -8,65 +8,77 @@ public class NewGame implements MouseMotionListener, MouseListener
     private GameArena arena;
     private Text[] numbers;
     private int counter = 0;
-    private int ballIndex = 1;
+    // private int ballIndex = 1;
     private boolean mouseDown = false;
-    private Rectangle R = new Rectangle(900, 300, 250,600, "Grey");
+    private Rectangle R = new Rectangle(900, 300, 280,600, "Grey");
     private Ball bc = new Ball(830,550, 21, "FireBrick");
     private Line l = new Line(385, 450,385, 560, 2, "AntiqueWhite");
-    private Text ballcount = new Text("x"+ballIndex, 875, 553, 15, "black");
+    private int lostBallsCounter = 0;
+
     public NewGame(){
         arena = new GameArena(1020, 600);
-        bricks = new Rectangle [85];
-        numbers = new Text[85];
+        bricks = new Rectangle [90];
+        numbers = new Text[90];
         arena.getPanel().addMouseListener(this);
         arena.getPanel().addMouseMotionListener(this);
         CreateBricks a = new CreateBricks();
         a.createBricks(bricks, arena, numbers, R, l, bc);
-        bb = new BallBounce[5];
-        for(int i = 0; i< 5; i++){
-            bb[i]= new BallBounce(arena);     //this is an array of ballbounce which therefore creates 5 balls
+        bb = new BallBounce[15];
+        for(int i = 0; i< 15; i++){
+            bb[i]= new BallBounce(arena);     //this is an array of ballbounce which creating 15 balls
         }
         arena.update();
 
         ballMovements();
     }
+
     private void ballMovements(){
         while(true){
-            arena.addText(ballcount);
             arena.update();
+            //when the mouse is down the game begins
             if(mouseDown){
                 arena.removeLine(l);
-                for(int i = 0; i< ballIndex; i++){
-                    if (counter/8 -1 >= i )
+                lostBallsCounter = 0;
+                for(int i = 0; i<15; i++){			//for loop which goes through all of the balls and releases them to move
+                    if (counter/8 -1 >= i ){				//the counter stops all of the balls from releasing at the same time
                         bb[i].ballmove(bricks, arena, numbers, R, l, bc);
-                    //System.out.println(ballIndex);
-                }
-                if(ballIndex<5){
-                    ballIndex++;
+                        if(bb[i].getIsBallLost())		//every time a ball is lost from the screen, the counter increments by one.
+                            lostBallsCounter++;
+                        if(lostBallsCounter==15){ 		//once all 15 balls have been lost from the screen, the balls are reset.
+                            arena.addLine(l);
+                            resetBalls();
+                            mouseDown=false;
+                            counter = 0;
+
+                            break;
+                        }
+                    }
                 }
                 counter++;
             }
         }
     }
     public void mouseMoved(MouseEvent e) {
-        l.setStart(e.getX(),  e.getY());            //sets the end of the line to the location of the mouse
+        l.setStart(e.getX(),  e.getY());			//sets the end of the line to the location of the mouse
     }
 
     public void mouseDragged(MouseEvent e) {
     }
+
+    //this is a method which shoots the balls to the location of the mouse
     public void mouseClicked(MouseEvent e) {
+        mouseDown = true;
+
         double angle = Math.atan2(l.getEndY()-l.getStartY(),l.getEndX()-l.getStartX()) + Math.PI;
         double xSpeed = Math.cos(angle)*6;
         double ySpeed =  Math.sin(angle)*6;;
-        System.out.println("x="+xSpeed+",y="+ySpeed);
-        for(int i = 0; i< 5; i++){
+        for(int i = 0; i< 15; i++){
             bb[i].setXSpeed(xSpeed, ySpeed);
-            //sets the direction of the ball shooting to the location of the mouse
         }
     }
 
     public void mousePressed(MouseEvent e) {
-        mouseDown = true;
+
     }
 
     public void mouseReleased(MouseEvent e) {
@@ -76,6 +88,17 @@ public class NewGame implements MouseMotionListener, MouseListener
     }
 
     public void mouseExited(MouseEvent e) {
+    }
+
+
+
+    //this is a method which will reset the game
+    public void resetBalls(){
+        for (int i = 0; i<15; i++){
+            //bb[i].resetXSpeed();
+            //bb[i].resetYSpeed();
+            bb[i].resetLocation();
+        }
     }
 
 }
